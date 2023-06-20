b09e878461cd2a2ecf6ed8a8c9c01ef573c65054
petroslikidis <petros.likidis@gmail.com>
2023-06-20 17:13:43 +0200

    Added name sanitazion

--- src/MinIOS3Provider/src/MinIOS3Provider/Controllers/BucketController.cs
+++ src/MinIOS3Provider/src/MinIOS3Provider/Controllers/BucketController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using S3Provider.Controllers;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.Arm;
+using System.Text.RegularExpressions;
+using System.IO.Hashing;
 
 namespace MinIOS3Provider.Controllers
 {
@@ -15,12 +18,38 @@ namespace MinIOS3Provider.Controllers
 
         public override IActionResult CreateBucket([FromQuery(Name = "preferredName"), Required] string preferredName)
         {
+            //TODO
+            //1. Sanitize preferredName
+            //2. client.CreateBucket()
+            //3. client.CreatePolicy()
+            //4. client.CreateUser()
+
             throw new NotImplementedException();
         }
 
         public override IActionResult CreateFolder([FromRoute(Name = "id"), Required] string id, [FromQuery(Name = "preferredName"), Required] string preferredName)
         {
             throw new NotImplementedException();
+        }
+
+        private string SanitizeName(string name)
+        {
+            name = name.ToLower(); //Only lower charachters allowed
+            name = name.PadLeft(3, '0'); //At least 3 charachters long otherwise padd with zeros in the begining
+            name = Regex.Replace(name,"[^a-z0-9]", "-"); // make sure that only a-z or digit or hypen repleces all other to hypen 
+            name = name.StartsWith("-") || name.StartsWith("xn--") ? "x" + name:name; //make sure it starts with a character or a number and not xn--
+
+            if (name.Length > 63) // Make sure that the name is not longer than 63 characters
+            {
+                System.IO.Hashing.Crc32 crc32 = new System.IO.Hashing.Crc32();
+
+                crc32.Append(System.Text.Encoding.ASCII.GetBytes(name));
+                var hash = crc32.GetCurrentHash();
+                var crcHash = string.Join("", hash.Select(b => b.ToString("x2").ToLower()).Reverse());
+                name = name.Substring(0, 63 - crcHash.Length) + crcHash;
+            }
+            return name;
+
         }
     }
 }
