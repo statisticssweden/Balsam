0480fb5774c1919a8dcd4dd3eaca5de6bafb0367
scbalth <alexander.thorell@scb.se>
2023-10-03 21:43:39 +0200

    Added support for multiple consecuteive Alerts

--- src/BalsamUI/balsamui/src/Alerter/Alerter.tsx
+++ src/BalsamUI/balsamui/src/Alerter/Alerter.tsx
@@ -1,57 +1,100 @@
 import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Snackbar from '@mui/material/Snackbar';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import {selectMessage, postAlert } from '../Alerts/alertsSlice';
+import {selectAlerts, postAlert, AlertItem, removeAlert } from '../Alerts/alertsSlice';
 
 // import ReduxHelper from '../ReduxHelper/ReduxHelper';
 
 export const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
         props,
-        ref) 
+        ref)
     {
         return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
     });
 
 export default function Alerter() {
-    const message = useSelector(selectMessage);
-    const [alertMessage, setAlertMessage] =useState("");
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const alerts = useSelector(selectAlerts);
+    const [currentAlert, setCurrentAlert] = useState<AlertItem>();
+    //const [alertMessage, setAlertMessage] = useState("");
+    // const [alertSeverity, setAlertSeverity] = useState("");
+    // const [alertId, setAlertId] = useState("");
+    //const [snackbarOpen, setSnackbarOpen] = useState(false);
     const dispatch = useDispatch();
 
-    const handleSnackbarClose = () => {
-        setSnackbarOpen(false);
-        //ReduxHelper.postAlert("");
-        dispatch(postAlert(""));
+    const handleSnackbarClose = (event, reason: SnackbarCloseReason) => {
         
+        if (reason !== "clickaway")
+        {
+            if(currentAlert !== undefined)
+            {
+                dispatch(removeAlert(currentAlert.id));
+            }
+        }
+    };
+
+    const handleAlertClose = () => {
+        
+        if(currentAlert !== undefined)
+        {
+            dispatch(removeAlert(currentAlert.id));
+        }
     };
 
     useEffect(() => {
-        if(message !== "")
+        if(alerts.length > 0)
         {
-            setAlertMessage(message);
-            setSnackbarOpen(true);
+            setCurrentAlert(alerts[0]);
+            // setSnackbarOpen(true);
+        }
+        else
+        {
+            setCurrentAlert(undefined);
+            //alert("då");
+            // setSnackbarOpen(false);
+        }
+    }, [alerts])
+
+    useEffect(() => {
+        // if(alerts.length > 0)
+        // {
+        //     setCurrentAlert(alerts[0]);
+        //     // setSnackbarOpen(true);
+        // }
+        // else
+        // {
+        //     setCurrentAlert(undefined);
+        //     //alert("då");
+        //     // setSnackbarOpen(false);
+        // }
+    }, [currentAlert])
+
+    const renderSnackbar = () =>
+    {
+        if (currentAlert !== undefined)
+        {
+
+            return <Snackbar anchorOrigin={{vertical: "top", horizontal: "center"}} open={true} autoHideDuration={6000} onClose={handleSnackbarClose}>
+                <Alert onClose={handleAlertClose} severity={currentAlert.severity} sx={{ width: '100%' }}>
+                {currentAlert.text}
+                </Alert>
+            </Snackbar>
 
         }
-        else 
+        else
         {
-            setSnackbarOpen(false);
+            //setSnackbarOpen(false);
         }
-    }, [message])
+    }
 
-    return <Snackbar anchorOrigin={{vertical: "top", horizontal: "center"}} open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
-        <Alert onClose={handleSnackbarClose} severity="success" sx={{ width: '100%' }}>
-            {alertMessage}
-        </Alert>
-    </Snackbar>
-
+    return renderSnackbar();
 }
 
 // export default function AppSnackBars(){
 //     // const [alertMessage, setAlertMessage]  = useState("");
 //     // const [snackbarOpen, setSnackbarOpen]  = useState(false);
 //     const [snackbars, setSnackbars] = useState([])
-    
+
 //     const handleSnackbarClose = () => {
 //         setSnackbarOpen(false);
 //     }
@@ -64,4 +107,5 @@ export default function Alerter() {
 //         </Alert>
 //     </Snackbar>
 // }
+
 
--- src/BalsamUI/balsamui/src/Alerts/alertsSlice.ts
+++ src/BalsamUI/balsamui/src/Alerts/alertsSlice.ts
@@ -1,24 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit'
+import {v4 as uuidv4} from 'uuid';
 
-// type alertMessage = {
-//   message: string
+export interface AlertItem {
+  id: string,
+  text: string,
+  severity: 'success' | 'error' | 'info' | 'warning',
+}
+
+// export interface MessagesState {
+//   messages: Array<MessageItemState>   
 // }
 
-export const alsertsSlice = createSlice({
+const initialState: Array<AlertItem> = [];
+
+
+export const alertsSlice = createSlice({
   name: 'alerts',
-  initialState: {
-    message: "",
-  },
+  initialState: initialState,
   reducers: {
     postAlert: (state, action) => {
-      state.message = action.payload
+      let newItem: AlertItem = { id: uuidv4(), severity: "success", text: action.payload};
+      state.push(newItem);
     },
-    
+    removeAlert: (state, action) => {
+      let id = action.payload;
+      const index = state.findIndex((item) => item.id === id);
+      state.splice(index, 1);
+    }
   },
 })
 
-export const { postAlert } = alsertsSlice.actions
+export const { postAlert, removeAlert } = alertsSlice.actions
 
-export const selectMessage = (state: any) => state.alerts.message;
+export const selectAlerts = (state: any) => state.alerts;
 
-export default alsertsSlice.reducer
+export default alertsSlice.reducer
