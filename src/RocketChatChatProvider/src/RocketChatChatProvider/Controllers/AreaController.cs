c077593993036ecae1408d82f24357bcfda603b7
petroslikidis <petros.likidis@gmail.com>
2023-10-06 10:55:28 +0200

    Added spec and project files

---
+++ src/RocketChatChatProvider/src/RocketChatChatProvider/Controllers/AreaController.cs
@@ -0,0 +1,13 @@
+﻿using ChatProvider.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RocketChatChatProvider.Controllers
+{
+    public class AreaController : ChatProvider.Controllers.AreaApiController
+    {
+        public override Task<IActionResult> CreateArea([FromBody] CreateAreaRequest? createAreaRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
