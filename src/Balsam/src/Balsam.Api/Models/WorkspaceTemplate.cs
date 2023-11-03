5bd355ec11e1bd14a208e15e1cb4f401ee4b496c
petroslikidis <petros.likidis@gmail.com>
2023-11-03 10:54:40 +0100

    Added url to response etc

--- src/Balsam/src/Balsam.Api/Models/WorkspaceTemplate.cs
+++ src/Balsam/src/Balsam.Api/Models/WorkspaceTemplate.cs
@@ -5,4 +5,5 @@ public class WorkspaceTemplate
     public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string UrlConfig { get; set; }
 }
