e8db61d1f07993e5f0af92dbc70bc5030a388d20
amar sepic <amar.sepic@scb.se>
2024-02-22 15:43:06 +0100

    feature to list knowledgelibraries

--- src/Balsam/src/Balsam.Api/Controllers/KnowledgeLibraryController.cs
+++ src/Balsam/src/Balsam.Api/Controllers/KnowledgeLibraryController.cs
@@ -1,5 +1,8 @@
-﻿using BalsamApi.Server.Models;
+﻿using Balsam.Api.Models;
+using BalsamApi.Server.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Options;
 using System.ComponentModel.DataAnnotations;
 
 namespace Balsam.Api.Controllers
@@ -8,9 +11,21 @@ namespace Balsam.Api.Controllers
     [ApiController]
     public class KnowledgeLibraryController : BalsamApi.Server.Controllers.KnowledgeLibraryApiController
     {
-        public override Task<IActionResult> ListKnowledgeLibaries()
+        private readonly HubClient _hubClient;
+        private readonly ILogger<ProjectController> _logger;
+
+        public KnowledgeLibraryController(IOptionsSnapshot<CapabilityOptions> capabilityOptions, ILogger<ProjectController> logger, HubClient hubClient)
         {
-            throw new NotImplementedException();
+            _hubClient = hubClient;
+            _logger = logger;
+            capabilityOptions.Get(Capabilities.Git);
+            capabilityOptions.Get(Capabilities.Authentication);
+        }
+        public async override Task<IActionResult> ListKnowledgeLibaries() //A. Implementera
+        {
+            _logger.LogInformation("calling endpoint: Listing Knowledgelibraries");
+            var knowledgeLibraries = await _hubClient.ListKnowledgelibraries();
+            return Ok(knowledgeLibraries);
         }
 
         public override Task<IActionResult> ListKnowledgeLibaryFileContent([FromRoute(Name = "libraryId"), Required] string libraryId, [FromRoute(Name = "fileId"), Required] string fileId)
--- src/Balsam/src/Balsam.Api/HubClient.cs
+++ src/Balsam/src/Balsam.Api/HubClient.cs
@@ -739,6 +739,25 @@ namespace Balsam.Api
 
         }
 
+        public async Task<List<KnowledgeLibrary>> ListKnowledgelibraries()
+        {
+            var knowledgeLibraries = new List<KnowledgeLibrary>();
+            var kbPath = Path.Combine(_hubRepositoryClient.Path, "kb");
+
+            foreach (var knowledgelibraryFile in Directory.GetFiles(kbPath))
+            {
+                var knowledgeLibrary = JsonConvert.DeserializeObject<KnowledgeLibrary>(await File.ReadAllTextAsync(knowledgelibraryFile));
+                if (knowledgeLibrary != null)
+                {
+                    knowledgeLibraries.Add(knowledgeLibrary);
+                }
+                else
+                {
+                    _logger.LogWarning($"Could not parse properties file for knowledgelibrary {knowledgelibraryFile}");
+                }
+            }
+            return knowledgeLibraries;
+        }
         internal async Task DeleteProject(string projectId)
         {
             var project = await GetProject(projectId);
